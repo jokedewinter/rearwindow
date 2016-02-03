2a6738a442866cec88baac27bf05e4e6fa2c80cb
Joke de Winter <joke@jokedewinter.co.uk>
2016-02-03 16:04:42 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<!--[if (lt IE 9) | IE 9 ]><html class="no-js ie" lang="en"> <![endif]-->
+<!--[if (gte IE 9) | !(IE)]><!--><html class="no-js" lang="en"> <!--<![endif]-->
+
+<head>
+	<title>Rear Window</title>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Trying something." />
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
+	<!-- Mobile IE allows us to activate ClearType technology for smoothing fonts for easy reading -->
+	<meta http-equiv="cleartype" content="on">
+
+	<link rel="stylesheet" href="css/screen.css">
+
+	<script>(function(){document.documentElement.className='js'})();</script>
+</head>
+
+<body>
+
+<div class="wrapper">
+
+	<header id="masthead" role="banner">
+		<h1>Rear Window</h1>
+	</header>
+
+	<main id="main" role="main">
+
+		<section class="rearwindow">
+			<ul class="rearwindow__areas">
+				<li id="purple">Behind the tree</li>
+				<li id="brown">The side view</li>
+				<li id="blue">Upstairs/Downstairs</li>
+				<li id="green">The courtyard</li>
+				<li id="red">Stacked rooms</li>
+				<li id="orange">Roof terraces</li>
+				<li id="grey">The tower block</li>
+				<li id="mint">The best apartment</li>
+				<li id="yellow">Down into the abyss</li>
+			</ul>
+
+			<ul class="rearwindow__images">
+				<li class="purple">
+					<a href="#purple">
+						<img src="images/colours/purple.png" alt="">
+					</a>
+				</li>
+
+				<li class="brown">
+					<a href="#brown">
+						<img src="images/colours/brown.png" alt="">
+					</a>
+				</li>
+
+				<li class="blue">
+					<a href="#blue">
+						<img src="images/colours/blue.png" alt="">
+					</a>
+				</li>
+
+				<li class="green">
+					<a href="#green">
+						<img src="images/colours/green.png" alt="">
+					</a>
+				</li>
+
+				<li class="red">
+					<a href="#red">
+						<img src="images/colours/red.png" alt="">
+					</a>
+				</li>
+
+				<li class="orange">
+					<a href="#orange">
+						<img src="images/colours/orange.png" alt="">
+					</a>
+				</li>
+
+				<li class="grey">
+					<a href="#grey">
+						<img src="images/colours/grey.png" alt="">
+					</a>
+				</li>
+
+				<li class="mint">
+					<a href="#mint">
+						<img src="images/colours/mint.png" alt="">
+					</a>
+				</li>
+
+				<li class="yellow">
+					<a href="#yellow">
+						<img src="images/colours/yellow.png" alt="">
+					</a>
+				</li>
+
+			</ul>
+
+			<img class="rearwindow__courtyard" src="images/courtyard.png" alt="">
+		</section>
+
+	</main><!-- /#main -->
+	<!--
+	<footer id="colophon" role="contentinfo">
+		<p>&copy; <?php echo date("Y"); ?> All rights reserved.</p>
+		<p class="jokedewinter"><a href="http://www.jokedewinter.co.uk" rel="external">Made by Joke De Winter</a></p>
+	</footer>
+-->
+
+</div><!-- /.wrapper -->
+</body>
+</html>
